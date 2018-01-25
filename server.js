3eca3d4292cfba40b6f8c17fd1dbd254954cd611
Derek Bocskey <bocskey.derek@gmail.com>
2018-01-25 01:13:12 -0500

    added to index and server files

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require('express');
-var method = require('method-override');
 var parser = require('body-parser');
+
+var app = express();
+
+var port = 3000;
+
+//pase app/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
